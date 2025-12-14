68101a257e6bbefe9b7bdb8393e56f2183333c9d
fatSheep <tzh2005t@163.com>
2025-12-14 23:38:08 +0800

    chore: format source code

--- mosaic.py
+++ mosaic.py
@@ -37,28 +37,35 @@ sys_crash = lambda: os.sys_crash()
 
 SYSCALLS = []
 
+
 def syscall(func):  # @syscall decorator
     SYSCALLS.append(func.__name__)
     return func
+
 
 ## 2. Mosaic operating system emulator
 
 ### 2.1 Data structures
 
+
 class Heap:
     pass  # no member: self.__dict__ is the heap
+
 
 @dataclass
 class Thread:
     context: Generator  # program counter, local variables, etc.
     heap: Heap  # a pointer to thread's "memory"
 
+
 @dataclass
 class Storage:
     persist: dict  # persisted storage state
     buf: dict  # outstanding operations yet to be persisted
 
+
 ### 2.2 The OperatingSystem class
+
 
 class OperatingSystem:
     """An executable operating system model.
@@ -170,7 +177,7 @@ class OperatingSystem:
         self._threads = [Thread(context=init(), heap=Heap())]
         self._current = 0
         self._choices = {init.__name__: lambda: None}
-        self._stdout = ''
+        self._stdout = ""
         self._storage = Storage(persist={}, buf={})
 
         # Internal states
@@ -178,13 +185,14 @@ class OperatingSystem:
         self._trace = []
         self._newfork = set()
 
-### 2.3 System call implementation
+    ### 2.3 System call implementation
 
-#### 2.3.1 Process, thread, and context switching
+    #### 2.3.1 Process, thread, and context switching
 
     @syscall
     def sys_spawn(self, func: Callable, *args):
         """Spawn a heap-sharing new thread executing func(args)."""
+
         def do_spawn():
             self._threads.append(
                 Thread(
@@ -192,22 +200,25 @@ class OperatingSystem:
                     heap=self.current().heap,  # shared heap
                 )
             )
-        return {'spawn': (lambda: do_spawn())}
+
+        return {"spawn": (lambda: do_spawn())}
 
     @syscall
     def sys_fork(self):
         """Create a clone of the current thread with a copied heap."""
-        if all(not f.frame.f_locals['fork_child']
-                for f in inspect.stack()
-                    if f.function == '_step'):  # this is parent; do fork
+        if all(
+            not f.frame.f_locals["fork_child"]
+            for f in inspect.stack()
+            if f.function == "_step"
+        ):  # this is parent; do fork
             # Deep-copying generators causes troubles--they are twined with
-            # Python's runtime state. We use an (inefficient) hack here: replay 
+            # Python's runtime state. We use an (inefficient) hack here: replay
             # the trace and override the last fork to avoid infinite recursion.
             os_clone = OperatingSystem(self._init)
             os_clone.replay(self._trace[:-1])
             os_clone._step(self._trace[-1], fork_child=True)
 
-            # Now os_clone._current is the forked process. Cloned thread just 
+            # Now os_clone._current is the forked process. Cloned thread just
             # yields a sys_fork and is pending for fork()'s return value. It
             # is necessary to mark cloned threads (in self._newfork) and send
             # child's fork() return value when they are scheduled for the
@@ -217,63 +228,71 @@ class OperatingSystem:
                 self._newfork.add((pid := len(self._threads)) - 1)
                 return 1000 + pid  # returned pid starts from 1000
 
-            return {'fork': (lambda: do_fork())}
+            return {"fork": (lambda: do_fork())}
         else:
             return None  # overridden fork; this value is never used because
-                         # os_clone is destroyed immediately after fork()
+            # os_clone is destroyed immediately after fork()
 
     @syscall
     def sys_sched(self):
         """Return a non-deterministic context switch to a runnable thread."""
         return {
-            f't{i+1}': (lambda i=i: self._switch_to(i))
-                for i, th in enumerate(self._threads)
-                    if th.context.gi_frame is not None  # thread still alive?
+            f"t{i+1}": (lambda i=i: self._switch_to(i))
+            for i, th in enumerate(self._threads)
+            if th.context.gi_frame is not None  # thread still alive?
         }
 
-### 2.3.2 Virtual character device (byte stream)
+    ### 2.3.2 Virtual character device (byte stream)
 
     @syscall
     def sys_choose(self, choices):
         """Return a non-deterministic value from choices."""
-        return {f'choose {c}': (lambda c=c: c) for c in choices}
+        return {f"choose {c}": (lambda c=c: c) for c in choices}
 
     @syscall
     def sys_write(self, *args):
         """Write strings (space separated) to stdout."""
-        def do_write():
-            self._stdout += ' '.join(str(arg) for arg in args)
-        return {'write': (lambda: do_write())}
 
-### 2.3.3 Virtual block storage device
+        def do_write():
+            self._stdout += " ".join(str(arg) for arg in args)
+
+        return {"write": (lambda: do_write())}
+
+    ### 2.3.3 Virtual block storage device
 
     @syscall
     def sys_bread(self, key):
         """Return the specific key's associated value in block device."""
         storage = self._storage
-        return {'bread': (lambda:
-            storage.buf.get(key,  # always try to read from buffer first
-                storage.persist.get(key, None)  # and then persistent storage
+        return {
+            "bread": (
+                lambda: storage.buf.get(
+                    key,  # always try to read from buffer first
+                    storage.persist.get(key, None),  # and then persistent storage
+                )
             )
-        )}
+        }
 
     @syscall
     def sys_bwrite(self, key, value):
         """Write (key, value) pair to block device's buffer."""
+
         def do_bwrite():
             self._storage.buf[key] = value
-        return {'bwrite': (lambda: do_bwrite())}
+
+        return {"bwrite": (lambda: do_bwrite())}
 
     @syscall
     def sys_sync(self):
         """Persist all buffered writes."""
+
         def do_sync():
             store = self._storage
             self._storage = Storage(
-                persist=store.persist | store.buf,  # write back
-                buf={}
+                persist=store.persist | store.buf, buf={}  # write back
             )
-        return {'sync': (lambda: do_sync())}
+
+        return {"sync": (lambda: do_sync())}
 
     @syscall
     def sys_crash(self):
@@ -284,20 +303,20 @@ class OperatingSystem:
         btrace = self._storage.buf.items()  # block trace
 
         crash_sites = (
-            lambda subset=subset:
-                setattr(self, '_storage',
-                    Storage(  # persist only writes in the subset
-                        persist=persist | dict(compress(btrace, subset)),
-                        buf={}
-                    )
-                ) for subset in  # Mosaic allows persisting any subset of
-                    product(     # pending blocks in the buffer
-                        *([(0, 1)] * len(btrace))
-                    )
+            lambda subset=subset: setattr(
+                self,
+                "_storage",
+                Storage(  # persist only writes in the subset
+                    persist=persist | dict(compress(btrace, subset)), buf={}
+                ),
+            )
+            for subset in product(  # Mosaic allows persisting any subset of  # pending blocks in the buffer
+                *([(0, 1)] * len(btrace))
+            )
         )
         return dict(enumerate(crash_sites))
 
-### 2.4 Operating system as a state machine
+    ### 2.4 Operating system as a state machine
 
     def replay(self, trace: list) -> dict:
         """Replay an execution trace and return the resulting state."""
@@ -322,7 +341,7 @@ class OperatingSystem:
         #   (self._threads, self._current, self._stdout, self._storage)
         # and outgoing transitions are saved in self._choices.
 
-### 2.5 Misc and helper functions
+    ### 2.5 Misc and helper functions
 
     def state_dump(self) -> dict:
         """Create a serializable Mosaic state dump with hash code."""
@@ -332,29 +351,31 @@ class OperatingSystem:
                 heaps[i] = len(heaps) + 1
 
         os_state = {
-            'current': self._current,
-            'choices': sorted(list(self._choices.keys())),
-            'contexts': [
-                {
-                    'name': th.context.gi_frame.f_code.co_name,
-                    'heap': heaps[id(th.heap)],  # the unique heap id
-                    'pc': th.context.gi_frame.f_lineno,
-                    'locals': th.context.gi_frame.f_locals.copy(),
-                } if th.context.gi_frame is not None else None
-                    for th in self._threads
+            "current": self._current,
+            "choices": sorted(list(self._choices.keys())),
+            "contexts": [
+                (
+                    {
+                        "name": th.context.gi_frame.f_code.co_name,
+                        "heap": heaps[id(th.heap)],  # the unique heap id
+                        "pc": th.context.gi_frame.f_lineno,
+                        "locals": th.context.gi_frame.f_locals.copy(),
+                    }
+                    if th.context.gi_frame is not None
+                    else None
+                )
+                for th in self._threads
             ],
-            'heaps': {
-                heaps[id(th.heap)]: th.heap.__dict__
-                    for th in self._threads
-            },
-            'stdout': self._stdout,
-            'store_persist': self._storage.persist,
-            'store_buffer': self._storage.buf,
+            "heaps": {heaps[id(th.heap)]: th.heap.__dict__ for th in self._threads},
+            "stdout": self._stdout,
+            "store_persist": self._storage.persist,
+            "store_buffer": self._storage.buf,
         }
 
         h = hash(json.dumps(os_state, sort_keys=True)) + 2**63
-        return (copy.deepcopy(os_state)  # freeze the runtime state
-                | dict(hashcode=f'{h:016x}'))
+        return copy.deepcopy(os_state) | dict(  # freeze the runtime state
+            hashcode=f"{h:016x}"
+        )
 
     def current(self) -> Thread:
         """Return the current running thread object."""
@@ -362,13 +383,15 @@ class OperatingSystem:
 
     def _switch_to(self, tid: int):
         self._current = tid
-        globals()['os'] = self
-        globals()['heap'] = self.current().heap
+        globals()["os"] = self
+        globals()["heap"] = self.current().heap
         if tid in self._newfork:
             self._newfork.remove(tid)  # tricky: forked process must receive 0
-            return 0                   # to indicate a child
+            return 0  # to indicate a child
+
 
 ## 3. The Mosaic runtime
+
 
 class Mosaic:
     """The operating system interpreter and model checker.
@@ -389,27 +412,28 @@ class Mosaic:
     - vertices: A list of operating system state dumps. The first vertex
                 in the list is the initial state. Each vertex has a
                 unique "hashcode" id.
-    - edges:    A list of 3-tuples: (source, target, label) denoting an 
+    - edges:    A list of 3-tuples: (source, target, label) denoting an
                 explored source --[label]-> target edge. Both source and
                 target are state hashcode ids.
     """
 
-### 3.1 Model interpreter and checker
+    ### 3.1 Model interpreter and checker
 
     def run(self) -> dict:
         """Interpret the model with non-deterministic choices."""
         os = OperatingSystem(self.entry)
         V, E = [os.state_dump() | dict(depth=0)], []
 
-        while (choices := V[-1]['choices']):
+        while choices := V[-1]["choices"]:
             choice = random.choice(choices)  # uniformly at random
             V.append(os.replay([choice]) | dict(depth=len(V)))
-            E.append((V[-2]['hashcode'], V[-1]['hashcode'], choice))
+            E.append((V[-2]["hashcode"], V[-1]["hashcode"], choice))
 
         return dict(source=self.src, vertices=V, edges=E)
 
     def check(self) -> dict:
         """Exhaustively explore the state space."""
+
         class State:
             entry = self.entry
 
@@ -417,11 +441,11 @@ class Mosaic:
                 self.trace = trace
                 self.state = OperatingSystem(State.entry).replay(trace)
                 self.state |= dict(depth=0)
-                self.hashcode = self.state['hashcode']
+                self.hashcode = self.state["hashcode"]
 
             def extend(self, c):
                 st = State(self.trace + (c,))
-                st.state = st.state | dict(depth=self.state['depth'] + 1)
+                st.state = st.state | dict(depth=self.state["depth"] + 1)
                 return st
 
         st0 = State(tuple())  # initial state of empty trace
@@ -429,7 +453,7 @@ class Mosaic:
 
         while queued:
             st = queued.pop(0)
-            for choice in st.state['choices']:
+            for choice in st.state["choices"]:
                 st1 = st.extend(choice)
                 if st1.hashcode not in V:  # found an unexplored state
                     V[st1.hashcode] = st1.state
@@ -438,23 +462,26 @@ class Mosaic:
 
         return dict(
             source=self.src,
-            vertices=sorted(V.values(), key=lambda st: st['depth']),
-            edges=E
+            vertices=sorted(V.values(), key=lambda st: st["depth"]),
+            edges=E,
         )
 
-### 3.1 Source code parsing and rewriting
+    ### 3.1 Source code parsing and rewriting
 
     class Transformer(ast.NodeTransformer):
         def visit_Call(self, node):
             # Rewrite system calls as yields
-            if (isinstance(node.func, ast.Name) and
-                    node.func.id in SYSCALLS):  # rewrite system calls
-                return ast.Yield(ast.Tuple(     #   -> yield ('sys_xxx', args)
-                    elts=[
-                        ast.Constant(value=node.func.id),
-                        ast.Tuple(elts=node.args),
-                    ]
-                ))
+            if (
+                isinstance(node.func, ast.Name) and node.func.id in SYSCALLS
+            ):  # rewrite system calls
+                return ast.Yield(
+                    ast.Tuple(  #   -> yield ('sys_xxx', args)
+                        elts=[
+                            ast.Constant(value=node.func.id),
+                            ast.Tuple(elts=node.args),
+                        ]
+                    )
+                )
             else:
                 return node
 
@@ -468,20 +495,20 @@ class Mosaic:
         globals().update(context)
 
         self.src = src
-        self.entry = context['main']  # must have a main()
+        self.entry = context["main"]  # must have a main()
+
 
 ## 4. Utilities
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     parser = argparse.ArgumentParser(
-        description='The modeled operating system and state explorer.'
+        description="The modeled operating system and state explorer."
     )
     parser.add_argument(
-        'source',
-        help='application code (.py) to be checked; must have a main()'
+        "source", help="application code (.py) to be checked; must have a main()"
     )
-    parser.add_argument('-r', '--run', action='store_true')
-    parser.add_argument('-c', '--check', action='store_true')
+    parser.add_argument("-r", "--run", action="store_true")
+    parser.add_argument("-c", "--check", action="store_true")
     args = parser.parse_args()
 
     src = Path(args.source).read_text()
